15755abe0ba50e8a310ad52fbb81bf8d5ea7a0a5
David Duarte <dduarte@zetti.com.ar>
2025-09-22 21:56:40 -0300

    Monedas desaparecen cada 4 segundos + Decrement gradual de dopamina

--- the-scrolling-dead/src/minigames/minigame.gd
+++ the-scrolling-dead/src/minigames/minigame.gd
@@ -4,11 +4,18 @@ extends Node2D
 @onready var spawn_timer: Timer = Timer.new()
 
 func _ready() -> void:
-	spawn_timer.wait_time = 1.0  # cada 1 segundo
-	spawn_timer.autostart = true
-	spawn_timer.one_shot = false
+	randomize()
+
+	build_timer(spawn_timer, 1.0)
 	add_child(spawn_timer)
 	spawn_timer.timeout.connect(_spawn_coin)
+
+
+func build_timer(timer: Timer, wait_time: float) -> void:
+	timer.wait_time = wait_time
+	timer.one_shot = false
+	timer.autostart = true
+
 
 func _spawn_coin() -> void:
 	if coin_scene:
@@ -18,5 +25,11 @@ func _spawn_coin() -> void:
 		var viewport_size = get_viewport().get_visible_rect().size
 		var random_x = randf_range(0, viewport_size.x)
 		var random_y = randf_range(0, viewport_size.y)
-
 		coin.position = Vector2(random_x, random_y)
+
+		var disappear_timer = Timer.new()
+		disappear_timer.wait_time = 4.0
+		disappear_timer.one_shot = true
+		disappear_timer.autostart = true
+		coin.add_child(disappear_timer)
+		disappear_timer.timeout.connect(Callable(coin, "queue_free"))
--- the-scrolling-dead/src/test_screen.gd
+++ the-scrolling-dead/src/test_screen.gd
@@ -1,9 +1,20 @@
 extends Node2D
 
+@onready var timer: Timer = Timer.new()
+
 func _ready():
 	DopamineManager.connect("game_over", Callable(self, "_on_dopamine_manager_game_over"))
 	DopamineManager.connect("on_target", Callable(self, "_on_dopamine_manager_on_target"))
 	DopamineManager.connect("target_changed", Callable(self, "_on_dopamine_manager_target_changed"))
+	
+	timer.wait_time = 2  # cada 2 segundos
+	timer.one_shot = false
+	timer.autostart = true
+	add_child(timer)
+	timer.timeout.connect(_on_timer_timeout)
+
+func _on_timer_timeout() -> void:
+	DopamineManager.decrement(10)
 
 func _on_inc_pressed() -> void:
 	DopamineManager.increment(10) 
