d08cc23fc035ef75a9c22e4e7ef80ec2080faad3
Leonardo Criado <70546903+LeonardoCriado@users.noreply.github.com>
2025-10-26 00:31:25 -0300

    Add animated background and update main menu UI
    Introduced new background and logo assets, replaced the main menu's static background and title with an animated sprite using a texture atlas. Updated button layout, added 'Options' and 'Credits' buttons, and switched to a pixel font for UI elements. The menu.gd script now starts the background animation on ready.

--- the-scrolling-dead/src/menu.gd
+++ the-scrolling-dead/src/menu.gd
@@ -1,9 +1,15 @@
 extends Control
 
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
+
+
 @export var main_scene: PackedScene
 
 func _ready():
 	$StartButton.pressed.connect(_on_start_pressed)
+	animated_sprite_2d.play()
 
 func _on_start_pressed():
 	get_tree().change_scene_to_packed(main_scene)
+	
+	
