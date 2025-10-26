5af4dba2b7c3bfd751e8c0369e563a26ac0c8b7c
Leonardo Criado <70546903+LeonardoCriado@users.noreply.github.com>
2025-10-26 03:30:43 -0300

    Add Credits screen and navigation
    Introduces a new Credits scene with its script and UI, updates main.tscn and menu.gd to support navigation to the Credits screen, and connects the relevant button signal for scene switching.

--- the-scrolling-dead/src/menu.gd
+++ the-scrolling-dead/src/menu.gd
@@ -3,6 +3,10 @@ extends Control
 @onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 @export var main_scene: PackedScene
 @export var option_scene: PackedScene
+@export var credit_scene: PackedScene
+@onready var audio_stream_player: AudioStreamPlayer = $AudioStreamPlayer
+
+
 
 func _ready():
 	$StartButton.pressed.connect(_on_start_pressed)
@@ -13,3 +17,7 @@ func _on_start_pressed():
 	
 func _on_options_pressed() -> void:
 	get_tree().change_scene_to_packed(option_scene)
+
+
+func _on_credits_pressed() -> void:
+	get_tree().change_scene_to_packed(credit_scene)
